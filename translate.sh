6a77eaef538a4f6c3b3d73586f73a45ee01e591f
ddd <542260219@qq.com>
2024-03-30 13:27:55 +0800

    g2g askcos

--- translate.sh
+++ translate.sh
@@ -4,11 +4,11 @@ python retroformer/translate.py \
   --batch_size_val 8 \
   --shared_vocab True \
   --shared_encoder False \
-  --data_dir <data_dir> \
-  --intermediate_dir <intermediate_dir> \
-  --checkpoint_dir <checkpoint_dir> \
-  --checkpoint <checkpoint> \
-  --known_class True \
+  --data_dir data/uspto50k/ \
+  --intermediate_dir intermediate/ \
+  --checkpoint_dir checkpoint \
+  --checkpoint model_best.pt \
+  --known_class False \
   --beam_size 10 \
   --stepwise False \
   --use_template False
